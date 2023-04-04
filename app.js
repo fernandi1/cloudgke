d2cb1bf09959d6d0e20b44520f5bc75c0474d7c6
fernandi1 <114889210+fernandi1@users.noreply.github.com>
2023-04-04 14:13:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Fernandi Julian Winardi\n')
+    res.send('52020016\n')
+    res.send('Teknik Informatika\n')
 })
 
 app.listen(PORT, HOST)
